6b427a80e0cfa2f9a149165ebc91c7aeed27ddce
bayudha2 <45285806+bayudha2@users.noreply.github.com>
2021-10-19 19:38:56 +0700

    jajal ganti dashboard (#2)

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 export const PageDashboard = () => (
     <div>
         <h1>Let's Goo Dashboard </h1>
-        <h1>Test lagi</h1>
+        <h1>sjadakdskasndkjd</h1>
     </div>
 );
 

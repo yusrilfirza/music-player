52c4592c829f09e2acf2764081482e360b612132
Firza <firza.yusril@gmail.com>
2021-10-19 12:15:36 +0700

    fixing header layout (add profile and search button

--- src/components/layouts/header/index.js
+++ src/components/layouts/header/index.js
@@ -1,15 +1,28 @@
-import React from 'react';
-import { faChevronRight, faChevronLeft } from '@fortawesome/free-solid-svg-icons';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import {
+    faChevronRight, faChevronLeft, faSearch,
+} from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { DropdownProfile } from '../../partials/dropdown/profile';
 
 export const LayoutHeader = (props) => {
+    const [isOpen, setIsOpen] = useState(false);
     const { loginStatus } = props;
+
+    const onClickProfile = () => {
+        setIsOpen(!isOpen);
+    };
+
+    const renderLogo = () => (
+        <Link to="/dashboard" className="nav-brand h-full w-36 bg-main-bold_gray-half" />
+    );
 
     const renderHeaderItem = () => {
         let headerItem = (
-            <div className="pl-20 flex justify-between w-full">
+            <div className="pl-20 pr-10 flex justify-between w-full">
                 <div className="flex">
-                    <img alt="musicplay_logo" src={`${process.env.PUBLIC_URL}/assets/img/logo_musicplay.png`} />
+                    {renderLogo()}
                     <div className="grid grid-cols-2 gap-8 ml-8">
                         <button type="button">
                             <FontAwesomeIcon icon={faChevronLeft} className="text-white" />
@@ -19,12 +32,18 @@ export const LayoutHeader = (props) => {
                         </button>
                     </div>
                 </div>
+                <div className="flex items-center">
+                    <Link to="/search" className="bg-white rounded-full h-auto px-3 py-1 mr-8">
+                        <FontAwesomeIcon icon={faSearch} className="text-main-bold_gray-lite text-sm" />
+                    </Link>
+                    <DropdownProfile open={isOpen} onClick={onClickProfile} />
+                </div>
             </div>
         );
         if (!loginStatus) {
             headerItem = (
                 <div className="flex w-9/12 mx-auto justify-between">
-                    <img alt="musicplay_logo" src={`${process.env.PUBLIC_URL}/assets/img/logo_musicplay.png`} />
+                    {renderLogo()}
                     <div className="grid grid-cols-2 gap-4">
                         <button type="button" className="bg-main-bold_gray-half text-white font-semibold rounded-full px-5 py-2 border-2 border-main-dark_green-half">Sign up</button>
                         <button type="button" className="bg-main-dark_green-half text-white font-semibold rounded-full px-5 py-2">Log in</button>
@@ -36,7 +55,7 @@ export const LayoutHeader = (props) => {
     };
 
     return (
-        <header className={`py-8 bg-main-bold_gray-half flex ${!loginStatus ? 'border-b border-main-dark_green-half' : ''}`}>
+        <header className={`py-8 bg-main-bold_gray-half flex ${!loginStatus ? 'border-b border-main-dark_green-half' : 'w-full mr-72'}`}>
             {renderHeaderItem()}
         </header>
     );
---
+++ src/components/partials/dropdown/profile/index.js
@@ -0,0 +1,61 @@
+import React, { Fragment } from 'react';
+import { Link } from 'react-router-dom';
+import { Menu, Transition } from '@headlessui/react';
+import { faSortDown, faSignOutAlt, faUserAlt } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+export const DropdownProfile = () => (
+    <Menu as="div" className="relative inline-block">
+        <Menu.Button className="bg-black flex items-center rounded-full px-4 py-3 text-white shadow-sm">
+            <img alt="" src={`${process.env.PUBLIC_URL}/assets/img/kagome.png`} className="h-full mr-4 rounded-full w-7" />
+            <span>
+                Kagome
+                <FontAwesomeIcon icon={faSortDown} className="text-white ml-4 my-auto" />
+            </span>
+        </Menu.Button>
+        <Transition
+            as={Fragment}
+            enter="transition ease-out duration-200"
+            enterFrom="opacity-0 translate-y-1"
+            enterTo="opacity-100 translate-y-0"
+            leave="transition ease-in duration-150"
+            leaveFrom="opacity-100 translate-y-0"
+            leaveTo="opacity-0 translate-y-1"
+        >
+            <Menu.Items className="origin-top-right absolute z-10 right-0 mt-2 w-36 rounded-md bg-black focus:outline-none">
+                <div className="py-1" role="none">
+                    <Menu.Item>
+                        {
+                            ({ active }) => (
+                                <Link
+                                    to="/profile"
+                                    className={`text-white block mx-1 rounded-md hover:bg-main-bold_gray px-4 py-2 text-sm ${active ? 'bg-main-bold_gray' : ''}`}
+                                    id="menu-item-0"
+                                >
+                                    <FontAwesomeIcon icon={faUserAlt} className="text-white mr-4" />
+                                    <span>Profile</span>
+                                </Link>
+                            )
+                        }
+                    </Menu.Item>
+                    <Menu.Item>
+                        {
+                            ({ active }) => (
+                                <Link
+                                    to="/login"
+                                    className={`text-white block mx-1 rounded-md hover:bg-main-bold_gray px-4 py-2 text-sm ${active ? 'bg-main-bold_gray' : ''}`}
+                                    id="menu-item-0"
+                                >
+                                    <FontAwesomeIcon icon={faSignOutAlt} className="text-white mr-4" />
+                                    <span>Log out</span>
+                                </Link>
+                            )
+                        }
+                    </Menu.Item>
+                </div>
+            </Menu.Items>
+        </Transition>
+    </Menu>
+);
+
+export default { DropdownProfile };

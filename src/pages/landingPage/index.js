ae350df12854923be13279ddecce4e0318eeb8f4
bayudha2 <bayudha.bd@gmail.com>
2021-10-30 12:36:26 +0700

    add Features partial  to landingPage

--- src/pages/landingPage/index.js
+++ src/pages/landingPage/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import Hero from '../../components/partials/Hero';
+import Feature from '../../components/partials/Feature';
 
 export const PageLanding = () => (
-    <Hero />
+    <>
+        <Hero />
+        <Feature />
+    </>
 );
 
 export default PageLanding;
